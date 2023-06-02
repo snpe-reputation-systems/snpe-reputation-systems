a446836eca024f37636e6188b9252793e825daa3
Diego Sanchez <diego.sp10@outlook.es>
2023-06-02 16:07:45 +0200

    test_fix2

--- snpe_reputation_systems/tests/test_simulator_class.py
+++ snpe_reputation_systems/tests/test_simulator_class.py
@@ -5,17 +5,13 @@ from ..snpe_reputation_systems.simulations.simulator_class import BaseSimulator
 
 
 @pytest.fixture
-def generate_params():
-    '''Generates a dictionary of parameters for the simulator class'''
-    params = params = {
+def test_base_simulator():
+    params = {
         "review_prior": np.ones(5),
         "tendency_to_rate": 0.05,
         "simulation_type": "timeseries"
     }
-
-@pytest.fixture
-def test_base_simulator():
-    return BaseSimulator(generate_params())
+    return BaseSimulator(params)
     
 def test_base_simulator_init(test_base_simulator):
     assert test_base_simulator(np.ones(5)) == np.array([2, 2, 2, 2, 2])
