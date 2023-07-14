06066cb4f89cd61774827ca40a0190cec7b26cbb
Diego Sanchez <diego.sp10@outlook.es>
2023-07-14 09:47:21 +0100

    unit_tests

--- snpe_reputation_systems/tests/test_simulator_class.py
+++ snpe_reputation_systems/tests/test_simulator_class.py
@@ -1,4 +1,5 @@
 import pytest
+import hypothesis
 import numpy as np
 import pandas as pd
 from ..snpe_reputation_systems.simulations.simulator_class import BaseSimulator
@@ -15,13 +16,19 @@ def test_base_simulator():
     
 def test_convolve_prior_with_existing_reviews(test_base_simulator):
     
+    # Test of correct sum
     assert np.array_equal(test_base_simulator.convolve_prior_with_existing_reviews(np.ones(5)), np.array([2, 2, 2, 2, 2]))
 
+    # Input shape test (Assertion error)
     with pytest.raises(AssertionError):
-        test_base_simulator.convolve_prior_with_existing_reviews(np.ones(6))
+        test_base_simulator.convolve_prior_with_existing_reviews(np.array([1, 2, 3, 4, 5, 6]))
+        
+    # Return type test
+    result = test_base_simulator.convolve_prior_with_existing_reviews(np.ones(5)), np.array([2, 2, 2, 2, 2])
+    assert isinstance(result, np.ndarray)
 
-     
-# Function does not seem robust to inclussion of np.nan in the input array
-# Function does not seem robust to inclussion of negative values in the input array
-# Function does not seem robust to inclussion of float values in the input array
-# Add xfail statements
+    #Null input test
+    with pytest.raises(TypeError):
+        test_base_simulator.convolve_prior_with_existing_reviews(None)
+
+    
