e2f15e948a2e45ba76ef27675bf25194fed2a199
Diego Sanchez <diego.sp10@outlook.es>
2023-07-14 15:05:52 +0100

    example_tests_deleted

--- snpe_reputation_systems/tests/test_sum_a.py
+++
@@ -1,8 +0,0 @@
-from ..snpe_reputation_systems.simulations.sum_a import sum_a_func
-
-def test_sum_a():
-    assert sum_a_func(2, 3) == 5
-    assert sum_a_func(-1, 1) == 0
-    assert sum_a_func(0, 0) == 0
-    assert sum_a_func(10, -5) == 5
-
