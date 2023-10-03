9515d560e0e593bbbcef675d9e45edc92ca7adf5
disanchz <diego.sp10@outlook.es>
2023-10-03 20:29:38 +0200

    fixes_to_simulate_review_histogram

--- snpe_reputation_systems/tests/test_simulator_class.py
+++ snpe_reputation_systems/tests/test_simulator_class.py
@@ -12,8 +12,8 @@ from numpy import float64
 
 from ..snpe_reputation_systems.simulations.simulator_class import (
     BaseSimulator,
-    SingleRhoSimulator,
     DoubleRhoSimulator,
+    SingleRhoSimulator,
 )
 
 
